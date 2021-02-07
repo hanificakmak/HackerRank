3c9642a7072ad26f30ab627545ac8dbe1c2b5e83
hanificakmak <36795390+hanificakmak@users.noreply.github.com>
2021-02-08 01:58:02 +0300

    Update Loops.py

--- Python/Loops.py
+++ Python/Loops.py
@@ -1,8 +1,10 @@
+# If you are copying this code be aware of the blanks after "#".
+
 i = int(input())
 num = 0
 while (num < i):
     print(num*num)
     num += 1
 
-#for n in range (0, i):
-#   print(n**2)
+# for n in range (0, i):
+#    print(n**2)
